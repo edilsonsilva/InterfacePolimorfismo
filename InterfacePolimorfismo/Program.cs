18ad4ef1eca3f7c6496ee85286ac1b1cdde31ab5
Fernando Henrique <fernando.henrique@info.local>
2017-11-13 20:49:24 -0200

    Alteração método main

--- InterfacePolimorfismo/Program.cs
+++ InterfacePolimorfismo/Program.cs
@@ -6,7 +6,7 @@ namespace InterfacePolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá mundo!");
         }
     }
 }
