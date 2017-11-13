471e824c8f5cda4304cdfda05681166c87969b46
Edilson <edilson_js@yahoo.com.br>
2017-11-13 20:34:34 -0200

    Modificacoes no Console

--- InterfacePolimorfismo/Program.cs
+++ InterfacePolimorfismo/Program.cs
@@ -6,7 +6,7 @@ namespace InterfacePolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Base para um novo projeto");
         }
     }
 }
